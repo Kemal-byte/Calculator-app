2cdab2178e814b278f9e6e140e25e092042ae68d
Kemal <i.kemal.karabulut@gmail.com>
2022-09-28 16:18:21 +0200

    TODO's added

--- main.js
+++ main.js
@@ -14,18 +14,29 @@ const deleteBtn = document.getElementById("delete");
 const lastOperation = document.getElementById("lastOperation");
 const currentOperation = document.getElementById("currentOperation");
 
-console.log(
-  addition.innerHTML.trim() +
-    " " +
-    substraction.innerHTML.trim() +
-    " " +
-    multiplication.innerHTML.trim() +
-    " " +
-    dividing.innerHTML.trim() +
-    numbers[1].innerHTML
-);
-
 deleteBtn.addEventListener("click", deleteNumber);
 clearBtn.addEventListener("click", clearDisplay);
 pointBtn.addEventListener("click", addPoint);
 equal.addEventListener("click", evaluate);
+addition.addEventListener("click", add);
+substraction.addEventListener("click", substract);
+dividing.addEventListener("click", divide);
+multiplication.addEventListener("click", multiply);
+
+function deleteNumber() {
+  console.log("clicked on delete number");
+}
+function clearDisplay() {
+  console.log("clicked on clearDisplay");
+}
+function addPoint() {
+  console.log("cliked on point button");
+}
+function evaluate() {
+  console.log("cliked on equal button");
+}
+
+function add() {}
+function divide() {}
+function substract() {}
+function multiply() {}
