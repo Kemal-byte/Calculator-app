8733b1fe8e554612a329b1d94fc9eaa7e66508b3
Kemal <i.kemal.karabulut@gmail.com>
2022-09-29 20:27:52 +0200

    multiplication needs some work.

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-let lastNumber = "";
 let currentNumber = "";
 let total = 0;
+let lastOperator = "";
 
 const operators = document.querySelectorAll(".operators");
 const addition = document.getElementById("addition");
@@ -16,6 +16,8 @@ const deleteBtn = document.getElementById("delete");
 let lastOperation = document.getElementById("lastOperation");
 let currentOperation = document.getElementById("currentOperation");
 
+let lastNumber = Number(currentOperation.innerHTML);
+
 // numbers.addEventListener("click", numConc);
 deleteBtn.addEventListener("click", deleteNumber);
 clearBtn.addEventListener("click", clearDisplay);
@@ -26,16 +28,15 @@ substraction.addEventListener("click", substract);
 dividing.addEventListener("click", divide);
 multiplication.addEventListener("click", multiply);
 
-console.log(operators);
-
 const getNum = function (e) {
+  console.log(`${equal.innerHTML}`);
   console.log(`${e.target.id}`);
   currentOperation.innerHTML += e.target.id;
 };
 const getOperator = (e) => {
   console.log(`${e.target.id}`);
+  lastOperator = e.target.id;
 };
-
 for (let x of operators) {
   // it adds onclick function to operators
   x.addEventListener("click", getOperator);
@@ -47,26 +48,41 @@ for (let x of numbers) {
 
 function deleteNumber() {
   currentOperation.innerHTML = null;
-  console.log("clicked on delete number");
 }
 function clearDisplay() {
   currentOperation.innerHTML = "";
   lastOperation.innerHTML = "";
-  console.log("clicked on clearDisplay");
+  lastOperator = "";
 }
 function addPoint() {
   console.log("cliked on point button");
 }
 function evaluate() {
-  // console.log("cliked on equal button");
-  if (getOperator === "addition") {
-    add();
-  } else if (getOperator === "subtraction") {
-    substract();
-  } else if (getOperator === "dividing") {
-    divide();
-  } else if (getOperator === "multiplication") {
-    multiply();
+  let y = Number(currentOperation.innerHTML);
+  if (lastOperator === "addition") {
+    if (lastOperation.innerHTML && !currentOperation.innerHTML) {
+      lastOperation.innerHTML = Number(lastOperation.innerHTML) + y;
+    } else {
+      add();
+    }
+  } else if (lastOperator === "subtraction") {
+    if (lastOperation.innerHTML && !currentOperation.innerHTML) {
+      lastOperation.innerHTML = Number(lastOperation.innerHTML) - y;
+    } else {
+      substract();
+    }
+  } else if (lastOperator === "dividing") {
+    if (!lastOperation.innerHTML && !currentOperation.innerHTML) {
+      lastOperation.innerHTML = Number(lastOperation.innerHTML) / y;
+    } else {
+      divide();
+    }
+  } else if (lastOperator === "multiplication") {
+    if (lastOperation.innerHTML && !currentOperation.innerHTML) {
+      lastOperation.innerHTML = Number(lastOperation.innerHTML) * y;
+    } else {
+      multiply();
+    }
   }
 }
 
@@ -78,9 +94,9 @@ function add() {
     total =
       Number(lastOperation.innerHTML) + Number(currentOperation.innerHTML);
     lastOperation.innerHTML = total;
+    lastNumber = Number(currentNumber.innerHTML);
     currentOperation.innerHTML = "";
   }
-  // }
 }
 function substract() {
   if (lastOperation.innerHTML === "") {
@@ -94,24 +110,63 @@ function substract() {
   }
 }
 function divide() {
-  if (lastOperation.innerHTML === "") {
+  // console.log(`Last Operation is ${lastOperation.innerHTML}`);
+  if (lastOperation.innerHTML == "") {
     lastOperation.innerHTML = currentOperation.innerHTML;
     currentOperation.innerHTML = "";
   } else {
-    total =
-      Number(lastOperation.innerHTML) / Number(currentOperation.innerHTML);
-    lastOperation.innerHTML = total;
-    currentOperation.innerHTML = "";
+    if (currentOperation.innerHTML == "") {
+      lastOperation.innerHTML = lastOperation.innerHTML;
+    } else {
+      total =
+        Number(lastOperation.innerHTML) / Number(currentOperation.innerHTML);
+      lastOperation.innerHTML = total;
+      currentOperation.innerHTML = "";
+    }
   }
+  reset(lastOperation);
 }
 function multiply() {
-  if (lastOperation.innerHTML === "") {
+  if (lastOperation.innerHTML == "") {
     lastOperation.innerHTML = currentOperation.innerHTML;
     currentOperation.innerHTML = "";
   } else {
-    total =
-      Number(lastOperation.innerHTML) * Number(currentOperation.innerHTML);
-    lastOperation.innerHTML = total;
-    currentOperation.innerHTML = "";
+    if (currentOperation.innerHTML == "") {
+      lastOperation.innerHTML = lastOperation.innerHTML;
+    } else {
+      total =
+        Number(lastOperation.innerHTML) * Number(currentOperation.innerHTML);
+      lastOperation.innerHTML = total;
+      currentOperation.innerHTML = "";
+    }
+  }
+  reset(lastOperation);
+}
+
+//* if the result of the calculation is one of the following below, then we want the display to return to the original.
+function reset(getLastNumber) {
+  console.log("Inside the reset function");
+  if (
+    getLastNumber.innerHTML === "NaN" ||
+    getLastNumber.innerHTML === "Infinity"
+  ) {
+    console.log("===================");
+    getLastNumber.innerHTML = "";
   }
 }
+// function calcultaion() {
+//   if (lastOperation.innerHTML === "") {
+//     lastOperation.innerHTML = currentOperation.innerHTML;
+//     currentOperation.innerHTML = "";
+//   } else if (lastOperation.innerHTML && !currentOperation.innerHTML) {
+//     // console.log(`last element is (second) = ${lastNumber}`);
+//     let y = Number(lastOperation.innerHTML) * lastNumber;
+//     lastOperation.innerHTML = y;
+//   } else {
+//     total =
+//       Number(lastOperation.innerHTML) * Number(currentOperation.innerHTML);
+//     lastOperation.innerHTML = total;
+//     lastNumber = Number(currentOperation.innerHTML);
+//     currentOperation.innerHTML = "";
+//   }
+// }
